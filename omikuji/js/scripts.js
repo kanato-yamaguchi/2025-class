fa7d633c5519559ec148aa932af99640d9f2926a
LAPTOP-DSVVKA2N\user <c033ffw@yamaguchi-u.ac.jp>
2025-05-28 14:48:14 +0900

    おみくじを追加

---
+++ omikuji/js/scripts.js
@@ -0,0 +1,8 @@
+// This file is intentionally left blank.
+document.getElementById('draw').addEventListener('click',function(){
+    const omikujiResults = ['大吉','中吉','小吉','末吉','凶'];
+    const result = omikujiResults[
+        Math.floor(Math.random()*omikujiResults.length)];
+    document.getElementById('result').textContent =
+    `あなたの運勢は...${result}!`;
+});
