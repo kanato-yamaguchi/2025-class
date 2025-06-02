b585c93c94e042129c82d25acce39a22f2dd4e68
LAPTOP-DSVVKA2N\user <c033ffw@yamaguchi-u.ac.jp>
2025-06-03 08:51:00 +0900

    結果にカラーを付けた

--- omikuji/js/scripts.js
+++ omikuji/js/scripts.js
@@ -1,8 +1,11 @@
 // This file is intentionally left blank.
 document.getElementById('draw').addEventListener('click',function(){
-    const omikujiResults = ['大吉','中吉','小吉','末吉','凶'];
-    const result = omikujiResults[
-        Math.floor(Math.random()*omikujiResults.length)];
+    const omikujiResults = ['大吉','吉','中吉','小吉','末吉','凶','小凶','大凶'];
+    const color =["red","yellow","blue","gray","lime","navy","olive","silver"];
+    var tmp = Math.floor(Math.random()*omikujiResults.length)
+    const result = omikujiResults[tmp];
+    var x = document.getElementById('result');
+    x.style.color =color[tmp];
     document.getElementById('result').textContent =
     `あなたの運勢は...${result}!`;
 });
